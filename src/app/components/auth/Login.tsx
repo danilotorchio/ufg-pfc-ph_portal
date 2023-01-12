21d061dfef411d2a29bc8825e372e2f69c182e6a
Danilo Torchio <danilotorchio@gmail.com>
2023-01-12 11:00:29 -0300

    Implementação do login e registro usando o firebase.

--- src/app/components/auth/Login.tsx
+++ src/app/components/auth/Login.tsx
@@ -1,4 +1,6 @@
-import { type ParentComponent } from 'solid-js';
+import { createSignal, Show, type ParentComponent } from 'solid-js';
+import { createStore } from 'solid-js/store';
+
 import { A } from '@solidjs/router';
 
 import {
@@ -12,17 +14,40 @@ import {
 import { useFirebase } from '../../utils/firebase';
 import LogoGoogle from '../../../assets/media/logos/google-icon.svg';
 
+type FormData = {
+  email: string;
+  password: string;
+};
+
 const Login: ParentComponent = () => {
   const { auth } = useFirebase();
 
-  const handleSubmit = async () => {
-    try {
-      await setPersistence(auth, browserSessionPersistence).then(async () => {
-        await signInWithEmailAndPassword(auth, '', '');
-      });
-    } catch (error) {
-      console.error(error);
+  const [form, setForm] = createStore<FormData>({
+    email: '',
+    password: '',
+  });
+
+  const [loading, setLoading] = createSignal(false);
+
+  const updateFormField = (fieldName: 'email' | 'password') => {
+    return (event: Event) => setForm({ [fieldName]: (event.currentTarget as HTMLInputElement).value });
+  };
+
+  const handleSubmit = async (evt: SubmitEvent) => {
+    evt.preventDefault();
+    setLoading(true);
+
+    if (form.email.trim() !== '' && form.password.trim() !== '') {
+      try {
+        await setPersistence(auth, browserSessionPersistence).then(async () => {
+          await signInWithEmailAndPassword(auth, form.email, form.password);
+        });
+      } catch (error) {
+        console.error(error);
+      }
     }
+
+    setLoading(false);
   };
 
   const handleSiginWithGoogle = async () => {
@@ -38,13 +63,13 @@ const Login: ParentComponent = () => {
   // prettier-ignore
   return (
     <div class="w-lg-500px p-10 animate__animated animate__fadeIn">
-      <form class="form w-100" novalidate={true} id="kt_sign_in_form">
+      <form class="form w-100" novalidate={true} id="kt_sign_in_form" onSubmit={handleSubmit}>
         <div class="text-center mb-11">
           <h1 class="text-dark fw-bolder mb-3">Login 🚀</h1>
         </div>
         <div class="row g-3 mb-9">
           <div class="col-md-12">
-            <button type="button" class="btn btn-flex btn-outline btn-text-gray-700 btn-active-color-primary bg-state-light flex-center text-nowrap w-100" onclick={handleSiginWithGoogle}>
+            <button type="button" class="btn btn-flex btn-outline btn-text-gray-700 btn-active-color-primary bg-state-light flex-center text-nowrap w-100" onClick={handleSiginWithGoogle}>
               <img alt="Logo" src={LogoGoogle} class="h-15px me-3" />
               Login com Google
             </button>
@@ -54,14 +79,20 @@ const Login: ParentComponent = () => {
           <span class="w-150px text-gray-500 fw-semibold fs-7">Ou com e-mail</span>
         </div>
         <div class="fv-row mb-8">
-          <input type="text" placeholder="E-mail" name="email" autocomplete="off" class="form-control bg-transparent" />
+          <input type="text" placeholder="E-mail" name="email" autocomplete="off" class="form-control bg-transparent" value={form.email} onChange={updateFormField('email')} />
         </div>
         <div class="fv-row mb-8">
-          <input type="password" placeholder="Senha" name="password" autocomplete="off" class="form-control bg-transparent" />
+          <input type="password" placeholder="Senha" name="password" autocomplete="off" class="form-control bg-transparent" value={form.password} onChange={updateFormField('password')} />
         </div>
         <div class="d-grid mb-10">
-          <button type="button" id="kt_sign_in_submit" class="btn btn-primary" onclick={handleSubmit}>
-            <span class="indicator-label">Entrar</span>
+          <button type="submit" id="kt_sign_in_submit" class="btn btn-primary" disabled={loading()}>
+            <Show when={!loading()} fallback={
+              <span>
+                Aguarde... <span class="spinner-border spinner-border-sm align-middle ms-2"></span>
+              </span>
+            }>
+              <span>Entrar</span>
+            </Show>
           </button>
         </div>
         <div class="text-gray-500 text-center fw-semibold fs-6">
--- src/app/components/auth/Register.tsx
+++ src/app/components/auth/Register.tsx
@@ -1,15 +1,75 @@
-import { Component } from 'solid-js';
+import { Component, createSignal, Show } from 'solid-js';
+import { createStore } from 'solid-js/store';
+
 import { A } from '@solidjs/router';
+import { browserSessionPersistence, createUserWithEmailAndPassword, setPersistence } from 'firebase/auth';
+
+import { useFirebase } from '../../utils/firebase';
+
+type FormData = {
+  email: string;
+  password: string;
+};
 
 const Register: Component = () => {
+  const [loading, setLoading] = createSignal(false);
+  const { auth } = useFirebase();
+
+  const [form, setForm] = createStore<FormData>({
+    email: '',
+    password: '',
+  });
+
+  const updateFormField = (fieldName: 'email' | 'password') => {
+    return (event: Event) => setForm({ [fieldName]: (event.currentTarget as HTMLInputElement).value });
+  };
+
+  const handleSubmit = async (evt: SubmitEvent) => {
+    evt.preventDefault();
+    setLoading(true);
+
+    try {
+      await setPersistence(auth, browserSessionPersistence).then(
+        async () => await createUserWithEmailAndPassword(auth, form.email, form.password)
+      );
+    } catch (error) {
+      console.error(error);
+    }
+
+    setLoading(false);
+  };
+
+  // prettier-ignore
   return (
-    <div class="animate__animated animate__fadeIn">
-      <div class="text-gray-500 text-center fw-semibold fs-6">
-        Já possui cadastro?
-        <A href={'/auth/login'} class="link-primary">
-          <span></span> Login
-        </A>
-      </div>
+    <div class="w-lg-500px p-10 animate__animated animate__fadeIn">
+      <form class="form w-100" novalidate={true} id="kt_sign_up_form" onSubmit={handleSubmit}>
+        <div class="text-center mb-11">
+          <h1 class="text-dark fw-bolder mb-3">Registro 🚀</h1>
+        </div>
+        <div class="fv-row mb-8">
+          <input type="text" placeholder="E-mail" name="email" autocomplete="off" class="form-control bg-transparent" value={form.email} onChange={updateFormField('email')} />
+        </div>
+        <div class="fv-row mb-8">
+          <input type="password" placeholder="Senha" name="password" autocomplete="off" class="form-control bg-transparent" value={form.password} onChange={updateFormField('password')} />
+        </div>
+        <div class="d-grid mb-10">
+          <button type="submit" id="kt_sign_in_submit" class="btn btn-primary" disabled={loading()}>
+            <Show when={!loading()} fallback={
+              <span>
+                Aguarde... <span class="spinner-border spinner-border-sm align-middle ms-2"></span>
+              </span>
+            }>
+              <span>Registrar</span>
+            </Show>
+          </button>
+        </div>
+        <div class="text-gray-500 text-center fw-semibold fs-6">
+          Já possui cadastro?
+          <A href={'/auth/login'} class="link-primary">
+            <span></span> Entrar
+          </A>
+        </div>
+      </form>
     </div>
   );
 };
